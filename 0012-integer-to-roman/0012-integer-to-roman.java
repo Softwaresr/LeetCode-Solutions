221469599b3ce9cc1b519cfed6960d437c6be386
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-07-05 00:17:24 +0530

    Time: 2 ms (100%), Space: 44.7 MB (19.93%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,19 +1,85 @@
+// class Solution {
+//   public String intToRoman(int num) {
+//     StringBuilder sb = new StringBuilder();
+//     process(sb, '*','M', '*', num/1000);
+//     num = num % 1000;
+
+//     process(sb, 'M','C' ,'D', num/100);
+//     num = num % 1000;
+
+//     process(sb, 'C','X', 'L', num/10);
+//     num = num % 1000;
+
+//     process(sb, 'X' ,'I', 'V', num);
+    
+
+//     return sb.toString();
+//   }
+
+//   void process(StringBuilder sb,char major,char minor , char middle, int val){
+//     if(val <= 3){
+//         for(int i=1;i<=val;i++){
+//             sb.append(minor);
+
+//         }
+
+//     }else if(val == 4){
+//         sb.append(minor);
+//         sb.append(middle);
+
+//     }else if(val == 5){
+//         sb.append(middle);
+
+//     }
+//     else if(val <=8){
+//         sb.append(middle);
+//         for(int i = 6; i <= val; i++){
+//             sb.append(minor);
+
+//         }
+//     }else if(val == 9){
+//         sb.append(minor);
+//         sb.append(major);
+//     }
+
+// }
+// }
 class Solution {
   public String intToRoman(int num) {
-    final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    final String[] symbols = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
-                              "XL", "X",  "IX", "V",  "IV", "I"};
     StringBuilder sb = new StringBuilder();
 
-    for (int i = 0; i < values.length; ++i) {
-      if (num == 0)
-        break;
-      while (num >= values[i]) {
-        num -= values[i];
-        sb.append(symbols[i]);
-      }
-    }
+    process(sb, '*', 'M', '*', num / 1000);
+    num = num % 1000;
+
+    process(sb, 'M', 'C', 'D', num / 100);
+    num = num % 100;
+
+    process(sb, 'C', 'X', 'L', num / 10);
+    num = num % 10;
+
+    process(sb, 'X', 'I', 'V', num);
 
     return sb.toString();
+  }
+
+  void process(StringBuilder sb, char major, char minor, char middle, int val) {
+    if (val <= 3) {
+      for (int i = 1; i <= val; i++) {
+        sb.append(minor);
+      }
+    } else if (val == 4) {
+      sb.append(minor);
+      sb.append(middle);
+    } else if (val == 5) {
+      sb.append(middle);
+    } else if (val <= 8) {
+      sb.append(middle);
+      for (int i = 6; i <= val; i++) {
+        sb.append(minor);
+      }
+    } else if (val == 9) {
+      sb.append(minor);
+      sb.append(major);
+    }
   }
 }
