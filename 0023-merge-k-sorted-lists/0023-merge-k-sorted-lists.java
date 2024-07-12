3bc15f79a979d62b9873459f5f53d2d0a898c036
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-07-12 13:25:01 +0530

    Time: 4 ms (75.22%), Space: 44.5 MB (24.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,31 +1,43 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-  public ListNode mergeKLists(ListNode[] lists) {
-    ListNode dummy = new ListNode(0);
-    ListNode curr = dummy;
-    Queue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
 
-    for (final ListNode list : lists)
-      if (list != null)
-        minHeap.offer(list);
-
-    while (!minHeap.isEmpty()) {
-      ListNode minNode = minHeap.poll();
-      if (minNode.next != null)
-        minHeap.offer(minNode.next);
-      curr.next = minNode;
-      curr = curr.next;
+// use a PriorityQueue
+// O(n * k * log k) runtime, O(k) space, where n is the maximum number of nodes in each list, and k is the number of lists
+public class Solution {
+    public class LabeledListNode implements Comparable<LabeledListNode> {
+        public ListNode listNode;
+        public int label;
+        public LabeledListNode(ListNode node, int n) {
+            listNode = node;
+            label = n;
+        }
+        @Override
+        public int compareTo(LabeledListNode another) {
+            return this.listNode.val - another.listNode.val;
+        }
     }
-
-    return dummy.next;
-  }
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<LabeledListNode> pq = new PriorityQueue<LabeledListNode>(lists.length);
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                pq.offer(new LabeledListNode(lists[i], i));
+                lists[i] = lists[i].next;
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            LabeledListNode node = pq.poll();
+            cur.next = node.listNode;
+            cur = cur.next;
+            int n = node.label;
+            if (lists[n] != null) {
+                pq.offer(new LabeledListNode(lists[n], n));
+                lists[n] = lists[n].next;
+            }
+        }
+        return dummy.next;
+    }
 }
