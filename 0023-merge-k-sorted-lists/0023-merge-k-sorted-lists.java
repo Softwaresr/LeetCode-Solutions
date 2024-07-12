d598f7b01c5b39d97404bef8a23744d3ecc0db95
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-07-12 13:25:47 +0530

    Time: 1 ms (100%), Space: 44.7 MB (14.07%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,43 +1,51 @@
-
-// use a PriorityQueue
-// O(n * k * log k) runtime, O(k) space, where n is the maximum number of nodes in each list, and k is the number of lists
-public class Solution {
-    public class LabeledListNode implements Comparable<LabeledListNode> {
-        public ListNode listNode;
-        public int label;
-        public LabeledListNode(ListNode node, int n) {
-            listNode = node;
-            label = n;
-        }
-        @Override
-        public int compareTo(LabeledListNode another) {
-            return this.listNode.val - another.listNode.val;
-        }
-    }
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
         }
-        PriorityQueue<LabeledListNode> pq = new PriorityQueue<LabeledListNode>(lists.length);
-        for (int i = 0; i < lists.length; i++) {
-            if (lists[i] != null) {
-                pq.offer(new LabeledListNode(lists[i], i));
-                lists[i] = lists[i].next;
-            }
+        return mergeKListsHelper(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
         }
+        if (start + 1 == end) {
+            return merge(lists[start], lists[end]);
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKListsHelper(lists, start, mid);
+        ListNode right = mergeKListsHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-        while (!pq.isEmpty()) {
-            LabeledListNode node = pq.poll();
-            cur.next = node.listNode;
-            cur = cur.next;
-            int n = node.label;
-            if (lists[n] != null) {
-                pq.offer(new LabeledListNode(lists[n], n));
-                lists[n] = lists[n].next;
+        ListNode curr = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
             }
+            curr = curr.next;
         }
+        
+        curr.next = (l1 != null) ? l1 : l2;
+        
         return dummy.next;
     }
 }
