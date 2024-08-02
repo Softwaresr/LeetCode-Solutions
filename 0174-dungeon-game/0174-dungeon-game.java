4db57a36f440e1fdde6a2692a13c75822ec7e55d
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-02 22:37:37 +0530

    Time: 0 ms (100%), Space: 44.9 MB (63.58%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,18 +1,35 @@
 class Solution {
-  public int calculateMinimumHP(int[][] dungeon) {
-    final int m = dungeon.length;
-    final int n = dungeon[0].length;
-    int[][] dp = new int[m + 1][n + 1];
-    Arrays.stream(dp).forEach(A -> Arrays.fill(A, Integer.MAX_VALUE));
-    dp[m][n - 1] = 1;
-    dp[m - 1][n] = 1;
+    public int calculateMinimumHP(int[][] dungeon) {
+        int[][] cache = new int[dungeon.length][dungeon[0].length];
 
-    for (int i = m - 1; i >= 0; --i)
-      for (int j = n - 1; j >= 0; --j) {
-        dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
-        dp[i][j] = Math.max(dp[i][j], 1);
-      }
+        for(int[] c:cache)
+            Arrays.fill(c,-1);
+        return function(0,0,dungeon,cache);
+    }
 
-    return dp[0][0];
-  }
+    int function(int i,int j,int[][] mat,int[][] cache){
+
+        if(i>=mat.length || j>=mat[0].length) return Integer.MAX_VALUE;
+
+        if(cache[i][j] !=-1) return cache[i][j];
+
+        if(i==mat.length-1 && j==mat[0].length-1){
+            //  1 - any +ve = -ve
+            // 1 - any -ve = +ve;
+            return cache[i][j]= Math.max(1,1-mat[i][j]);
+        }
+
+        
+
+        int right = function(i,j+1,mat,cache);
+        int down = function(i+1,j,mat,cache);
+
+        int minEnergy = Math.min(right,down);
+
+        //decide energey level
+        // when current has sufficent energy to manage prev damage, 
+        //it will become-ve and 1 will be returned
+        return cache[i][j]=  Math.max(1,minEnergy-mat[i][j]);//energey required at curerent level
+
+    }
 }
