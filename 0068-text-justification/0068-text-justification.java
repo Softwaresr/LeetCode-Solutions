6221b0c9428116f184f0b3da72556e5f512f4f68
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-05 14:51:54 +0530

    Time: 0 ms (100%), Space: 41.7 MB (54.81%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,43 +1,56 @@
 class Solution {
-  public List<String> fullJustify(String[] words, int maxWidth) {
-    List<String> ans = new ArrayList<>();
-    List<StringBuilder> row = new ArrayList<>();
-    int rowLetters = 0;
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> list = new ArrayList<>();
+        int idx = 0;
+        int n = words.length;
 
-    for (final String word : words) {
-      // If we place the word in this row, it will exceed the maximum width.
-      // Therefore, we cannot put the word in this row and have to pad spaces
-      // for each word in this row.
-      if (rowLetters + row.size() + word.length() > maxWidth) {
-        final int spaces = maxWidth - rowLetters;
-        if (row.size() == 1) {
-          // Pad all the spaces after row[0].
-          for (int i = 0; i < spaces; ++i)
-            row.get(0).append(" ");
-        } else {
-          // Evenly pad all the spaces to each word (expect the last one) in
-          // this row.
-          for (int i = 0; i < spaces; ++i)
-            row.get(i % (row.size() - 1)).append(" ");
+        while (idx < n) {
+            int count = 0;
+            int currLength = 0;
+            int oldIdx = idx;
+            int totalWordLength = 0;
+
+            while (idx < n && currLength + words[idx].length() <= maxWidth) {
+                count++;
+                currLength += words[idx].length() + 1;
+                totalWordLength += words[idx].length();
+                idx++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            // Left Justify
+            if (count == 1 || idx == n) {
+                while (oldIdx < idx) {
+                    sb.append(words[oldIdx++]);
+                    if (!words[oldIdx-1].endsWith(".") && oldIdx != idx) {
+                        sb.append(" ");
+                    }
+                }
+
+                while (sb.length() < maxWidth) {
+                    sb.append(" ");
+                }
+            }
+            // Left & Right Justify
+            else {
+                int spacing = (maxWidth - totalWordLength) / (count - 1);
+                int extraSpacing = maxWidth - totalWordLength - spacing * (count - 1);
+                int tempIdx = oldIdx;
+                while (tempIdx < idx) {
+                    sb.append(words[tempIdx++]);
+                    if (tempIdx != idx) {
+                        int totalSpace = spacing + (extraSpacing > 0 ? 1 : 0);
+                        extraSpacing--;
+                        for (int j = 0; j < totalSpace; j++) {
+                            sb.append(" ");
+                        }
+                    }
+                }
+            }
+
+            list.add(sb.toString());
         }
-        final String joinedRow =
-            row.stream().map(StringBuilder::toString).collect(Collectors.joining(""));
-        ans.add(joinedRow);
-        row.clear();
-        rowLetters = 0;
-      }
-      row.add(new StringBuilder(word));
-      rowLetters += word.length();
+
+        return list;
     }
-
-    final String lastRow =
-        row.stream().map(StringBuilder::toString).collect(Collectors.joining(" "));
-    StringBuilder sb = new StringBuilder(lastRow);
-    final int spacesToBeAdded = maxWidth - sb.length();
-    for (int i = 0; i < spacesToBeAdded; ++i)
-      sb.append(" ");
-
-    ans.add(sb.toString());
-    return ans;
-  }
 }
