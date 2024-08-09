028856c7e1c2173ccd1c11b09b96dd32ed29a99c
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-09 23:40:06 +0530

    Time: 0 ms (100%), Space: 48.5 MB (12.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,37 @@
+import java.util.AbstractList;
 class Solution {
-  public List<List<String>> groupAnagrams(String[] strs) {
-    Map<String, List<String>> keyToAnagrams = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        return new AbstractList<List<String>>() {
+            List<List<String>> result;
+            public List<String> get(int index) {
+                if (result == null) {
+                    init();
+                }
 
-    for (final String str : strs) {
-      char[] chars = str.toCharArray();
-      Arrays.sort(chars);
-      String key = String.valueOf(chars);
-      keyToAnagrams.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+                return result.get(index);
+            }
+            public int size() {
+                if (result == null) {
+                    init();
+                }
+                return result.size();
+            }
+            private void init() {
+                for (String s: strs) {
+                    char[] keys = new char[26];
+                    for (int i = 0; i < s.length(); i++) {
+                        keys[s.charAt(i) - 'a']++;
+                    }
+                    String key = new String(keys);
+                    List<String> list = map.get(key);
+                    if (list == null) {
+                        map.put(key, new ArrayList<>());
+                    }
+                    map.get(key).add(s);
+                }
+                result = new ArrayList<>(map.values());
+            }
+        };
     }
-
-    return new ArrayList<>(keyToAnagrams.values());
-  }
 }
