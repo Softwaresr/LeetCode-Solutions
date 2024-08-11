c283df708d6afcdec98d224c4c7a01882663019a
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-11 22:42:02 +0530

    Time: 8 ms (99.75%), Space: 48.6 MB (11.7%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,58 +1,43 @@
-class T {
-  public int i;
-  public int j;
-  public int h; // heightMap[i][j] or the height after filling water
-  public T(int i, int j, int h) {
-    this.i = i;
-    this.j = j;
-    this.h = h;
-  }
-}
 
 class Solution {
-  public int trapRainWater(int[][] heightMap) {
-    final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    final int m = heightMap.length;
-    final int n = heightMap[0].length;
-    int ans = 0;
-    Queue<T> minHeap = new PriorityQueue<>((a, b) -> a.h - b.h);
-    boolean[][] seen = new boolean[m][n];
-
-    for (int i = 0; i < m; ++i) {
-      minHeap.offer(new T(i, 0, heightMap[i][0]));
-      minHeap.offer(new T(i, n - 1, heightMap[i][n - 1]));
-      seen[i][0] = true;
-      seen[i][n - 1] = true;
-    }
-
-    for (int j = 1; j < n - 1; ++j) {
-      minHeap.offer(new T(0, j, heightMap[0][j]));
-      minHeap.offer(new T(m - 1, j, heightMap[m - 1][j]));
-      seen[0][j] = true;
-      seen[m - 1][j] = true;
-    }
-
-    while (!minHeap.isEmpty()) {
-      final int i = minHeap.peek().i;
-      final int j = minHeap.peek().j;
-      final int h = minHeap.poll().h;
-      for (int[] dir : dirs) {
-        final int x = i + dir[0];
-        final int y = j + dir[1];
-        if (x < 0 || x == m || y < 0 || y == n)
-          continue;
-        if (seen[x][y])
-          continue;
-        if (heightMap[x][y] < h) {
-          ans += h - heightMap[x][y];
-          minHeap.offer(new T(x, y, h)); // Fill water in grid[x][y].
-        } else {
-          minHeap.offer(new T(x, y, heightMap[x][y]));
+    public int trapRainWater(int[][] heightMap) {
+        int rows = heightMap.length, cols = heightMap[0].length;
+        if(heightMap == null || rows < 3 || cols < 3)  return 0;        
+        int[][] water = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(heightMap[i], 0, water[i], 0, cols);
         }
-        seen[x][y] = true;
-      }
+        boolean updated = true, init = true;
+        while (updated) {
+            updated = false;
+            for (int i = 1; i < rows - 1; i++) {
+                for (int j = 1; j < cols - 1; j++) {
+                    int val = Math.max(heightMap[i][j], Math.min(water[i - 1][j], water[i][j - 1]));
+                    if (init || val < water[i][j]) {
+                        water[i][j] = val;
+                        updated = true;
+                    }
+                }
+            }
+            init = false;
+            for (int i = rows - 2; i > 0; i--) {
+                for (int j = cols - 2; j > 0; j--) {
+                    int val = Math.max(heightMap[i][j], Math.min(water[i + 1][j], water[i][j + 1]));
+                    if (val < water[i][j]) {
+                        water[i][j] = val;
+                        updated = true;
+                    }
+                }
+            }
+        }
+        int rs = 0;
+        for (int i = 1; i < rows - 1; i++) {
+            for (int j = 1; j < cols - 1; j++) {
+                if (water[i][j] - heightMap[i][j] > 0) {
+                    rs += water[i][j] - heightMap[i][j];
+                }
+            }
+        }
+        return rs;
     }
-
-    return ans;
-  }
 }
