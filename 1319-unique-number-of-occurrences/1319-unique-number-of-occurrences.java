a6513279b52e87c88aa0ae1b66aec4b78b25c9ad
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-13 00:21:19 +0530

    Time: 2 ms (97.99%), Space: 42.1 MB (20.51%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer , Integer> fmap = new HashMap<>();
-
-        for(int val: arr){
-            fmap.put(val, fmap.getOrDefault(val, 0) + 1);
+        HashMap<Integer, Integer> fmap = new HashMap<>();
+        for (int val: arr){
+            fmap.put(val, fmap.getOrDefault(val, 0)  + 1);
 
         }
         
-        HashSet<Integer> unique = new HashSet<>();
-        for(int freq: fmap.values()){
-            if(unique.contains(freq)){
-                return false;
+       HashSet<Integer> unique = new HashSet<>();
+       for(int freq: fmap.values()){
+        if (unique.contains(freq)){
+            return false;
 
-            }
-            else{
-                unique.add(freq); 
-            }
+        } 
+        else{
+            unique.add(freq);
         }
-        return true;
+
+       }
+       return true;
+       
     }
 }
