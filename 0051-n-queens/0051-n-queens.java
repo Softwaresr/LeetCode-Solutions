8daa4908c4922036cf85298df826d9856ca02018
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-16 14:37:31 +0530

    Time: 0 ms (100%), Space: 44.6 MB (87.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,37 +1,55 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 class Solution {
+  private int n;
+  private List<List<String>> builder;
+  private String[] template;
+  private String[] board;
   public List<List<String>> solveNQueens(int n) {
-    List<List<String>> ans = new ArrayList<>();
-    char[][] board = new char[n][n];
-
-    for (int i = 0; i < n; ++i)
-      Arrays.fill(board[i], '.');
-
-    dfs(n, 0, new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1], board, ans);
-    return ans;
+    return new AbstractList<List<String>>() {
+      private List<List<String>> cache = null;
+      @Override
+      public List<String> get(int index) {
+        if (cache == null) 
+        cache = solveNQueens1(n);
+        return cache.get(index);
+      }
+      @Override
+      public int size() {
+        if (cache == null) 
+        cache = solveNQueens1(n);
+        return cache.size();
+      }
+    };
   }
-
-  private void dfs(int n, int i, boolean[] cols, boolean[] diag1, boolean[] diag2, char[][] board,
-                   List<List<String>> ans) {
-    if (i == n) {
-      ans.add(construct(board));
-      return;
+  public List<List<String>> solveNQueens1(int n) {
+    this.n = n;
+    this.builder = new ArrayList<>();
+    this.template = new String[n];
+    this.board = new String[n];
+    char[] line = new char[n];
+    Arrays.fill(line, '.');
+    for (int i = 0; i < n; i++) {
+      line[i] = 'Q';
+      template[i] = String.valueOf(line);
+      line[i] = '.';
     }
-
-    for (int j = 0; j < cols.length; ++j) {
-      if (cols[j] || diag1[i + j] || diag2[j - i + n - 1])
-        continue;
-      board[i][j] = 'Q';
-      cols[j] = diag1[i + j] = diag2[j - i + n - 1] = true;
-      dfs(n, i + 1, cols, diag1, diag2, board, ans);
-      cols[j] = diag1[i + j] = diag2[j - i + n - 1] = false;
-      board[i][j] = '.';
-    }
+    enumerateRow(0, 0L, 0L, 0L);
+    return builder;
   }
-
-  private List<String> construct(char[][] board) {
-    List<String> listBoard = new ArrayList<>();
-    for (int i = 0; i < board.length; ++i)
-      listBoard.add(String.valueOf(board[i]));
-    return listBoard;
+  private void enumerateRow(int i, long shadow0, long shadow1, long shadow2) {
+    long shadowSum = shadow0 | shadow1 | shadow2;
+    for (int j = 0; j < n; j++) {
+      long cell = 1L << j;
+      if ((shadowSum & cell) == 0L) {
+        board[i] = template[j];
+        if (i == n - 1)
+        builder.add(new ArrayList<>(Arrays.asList(board)));
+        else
+        enumerateRow(i + 1, (shadow0 | cell) << 1, shadow1 | cell, (shadow2 | cell) >> 1);
+      }
+    }
   }
 }
