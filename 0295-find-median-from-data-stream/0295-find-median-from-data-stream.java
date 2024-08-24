a100036f9b70d65b7e2cfdc9bb85f87904d5171b
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-24 22:46:12 +0530

    Time: 71 ms (99.76%), Space: 62 MB (99.37%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,24 +1,100 @@
 class MedianFinder {
-  public void addNum(int num) {
-    if (maxHeap.isEmpty() || num <= maxHeap.peek())
-      maxHeap.offer(num);
-    else
-      minHeap.offer(num);
+    int[] hashVals;
+    int median;
+    int medianCount2x;
 
-    // Balance the two heaps s.t.
-    // |maxHeap| >= |minHeap| and |maxHeap| - |minHeap| <= 1.
-    if (maxHeap.size() < minHeap.size())
-      maxHeap.offer(minHeap.poll());
-    else if (maxHeap.size() - minHeap.size() > 1)
-      minHeap.offer(maxHeap.poll());
-  }
+    public MedianFinder() {
+        hashVals = new int[200001];
+        median = Integer.MIN_VALUE;
+        medianCount2x = 1;
+    }
+    
+    public void addNum(int num) {
+        hashVals[num+100000]++;
+        if (median != Integer.MIN_VALUE) {
+            if (num > median) {
+                if (medianCount2x < 2*hashVals[median+100000]) {
+                    medianCount2x++;
+                } else {
+                    medianCount2x = 1;
+                    while (hashVals[++median+100000] == 0) {}
+                }
+            } else if (num < median) {
+                if (medianCount2x > 1) {
+                    medianCount2x--;
+                } else {
+                    while (hashVals[--median + 100000] ==0) {}
+                    medianCount2x = 2*hashVals[median+100000];
+                }
+            } else {
+                medianCount2x++;
+            }
+        } else {
+            median = num;
+        }
+    }
+    
+    public double findMedian() {
+        if (medianCount2x % 2 == 0) {
+            if (2*hashVals[median+100000] > medianCount2x) {
+                return median;
+            } else {
+                int median2 = median;
+                while (hashVals[++median2 + 100000] == 0) {}
+                return (median + median2)/2.0;
+            }
+        } else {
+            return median;
+        }
+    }
+    /*
+    TreeMap<Integer,Integer> hash;
+    int medianInd;
+    int medianCount2x;
 
-  public double findMedian() {
-    if (maxHeap.size() == minHeap.size())
-      return (double) (maxHeap.peek() + minHeap.peek()) / 2.0;
-    return (double) maxHeap.peek();
-  }
-
-  private Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-  private Queue<Integer> minHeap = new PriorityQueue<>();
+    public MedianFinder() {
+        hash = new TreeMap<>();
+        medianInd = Integer.MIN_VALUE;
+        medianCount2x = 1;
+    }
+    
+    public void addNum(int num) {
+        hash.compute(num, (k,v) -> v == null ? 1 : v + 1);
+        if (medianInd != Integer.MIN_VALUE) {
+            if (num > medianInd) {
+                if (medianCount2x < 2*hash.get(medianInd)) {
+                    medianCount2x++;
+                } else {
+                    medianCount2x = 1;
+                    medianInd = hash.higherKey(medianInd);
+                }
+            } else if (num < medianInd) {
+                if (medianCount2x > 1) {
+                    medianCount2x--;
+                } else {
+                    Map.Entry<Integer, Integer> lower = hash.lowerEntry(medianInd);
+                    medianInd = lower.getKey();
+                    medianCount2x = 2*lower.getValue();
+                }
+            } else {
+                medianCount2x++;
+            }
+        } else {
+            medianInd = num;
+        }
+    }
+    
+    public double findMedian() {
+        if (medianCount2x % 2 == 0) {
+            int count = hash.get(medianInd);
+            if (2*count > medianCount2x) {
+                return medianInd;
+            } else {
+                return (medianInd + hash.higherKey(medianInd))/2.0;
+            }
+        } else {
+            return medianInd;
+        }
+    }
+    */
 }
