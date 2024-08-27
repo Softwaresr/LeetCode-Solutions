d2fd58112fcfa53d26525e9bc927704a5e72e941
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-27 22:44:53 +0530

    Time: 2 ms (100%), Space: 44.8 MB (68.8%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.java
+++ 0087-scramble-string/0087-scramble-string.java
@@ -1,40 +1,51 @@
 class Solution {
-  public boolean isScramble(String s1, String s2) {
-    if (s1.equals(s2))
-      return true;
-    final String hashKey = s1 + "+" + s2;
-    if (mem.containsKey(hashKey))
-      return mem.get(hashKey);
-
-    int[] count = new int[128];
-
-    for (int i = 0; i < s1.length(); ++i) {
-      ++count[s1.charAt(i)];
-      --count[s2.charAt(i)];
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        Boolean[][] memo = new Boolean[n][n];
+        return _isScramble(s1, s2, 0, 0, memo);
     }
 
-    for (final int freq : count)
-      if (freq != 0) {
-        mem.put(hashKey, false);
-        return false;
-      }
-
-    for (int i = 1; i < s1.length(); ++i) {
-      if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
-          isScramble(s1.substring(i), s2.substring(i))) {
-        mem.put(hashKey, true);
-        return true;
-      }
-      if (isScramble(s1.substring(0, i), s2.substring(s2.length() - i)) &&
-          isScramble(s1.substring(i), s2.substring(0, s2.length() - i))) {
-        mem.put(hashKey, true);
-        return true;
-      }
+    private boolean _isScramble(String src, String dst, int si, int di, Boolean[][] memo) {
+        int sn = src.length(), dn = dst.length();
+        if (sn == 1) {
+            return src.equals(dst);
+        } else if (!isAnagram(src, dst)) {
+            return false;
+        } else if (memo[si][di] != null) {
+            return memo[si][di];
+        }
+        // `i` is the splitter
+        for (int i = 1; i < sn; i++) {
+            String srcx = src.substring(0, i), srcy = src.substring(i);
+            // we check both swapped and unswapped here
+            //
+            // for example, given input: "abb", "bba", and i = 1
+            //
+            // _isScramble('a', 'b') && _isScramble('bb', 'ba')
+            // or
+            // _isScramble('a', 'a') && _isScramble('bb', 'bb')
+            //
+            if ((_isScramble(srcx, dst.substring(0, i), i, i, memo)
+                    && _isScramble(srcy, dst.substring(i), sn - i, i, memo))
+                || (_isScramble(srcx, dst.substring(dn - i), i, dn - i, memo)
+                    && _isScramble(srcy, dst.substring(0, dn - i), sn - i, dn - i, memo))) {
+                return memo[si][di] = true;
+            }
+        }
+        return memo[si][di] = false;
     }
 
-    mem.put(hashKey, false);
-    return false;
-  }
-
-  private Map<String, Boolean> mem = new HashMap<>();
+    private boolean isAnagram(String src, String dst) {
+        int[] count = new int[26];
+        for (int i = 0; i < src.length(); i++) {
+            count[src.charAt(i) - 'a']++;
+            count[dst.charAt(i) - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
