f7fe2d4c840b758dfeae8a503da34500ebbc7d1a
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-08-30 23:45:56 +0530

    Time: 2 ms (96.74%), Space: 43.8 MB (95.76%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,29 +1,28 @@
 class Solution {
-  public boolean isMatch(String s, String p) {
-    final int m = s.length();
-    final int n = p.length();
-    // dp[i][j] := true if s[0..i) matches p[0..j)
-    boolean[][] dp = new boolean[m + 1][n + 1];
-    dp[0][0] = true;
-
-    for (int j = 0; j < p.length(); ++j)
-      if (p.charAt(j) == '*')
-        dp[0][j + 1] = dp[0][j];
-
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (p.charAt(j) == '*') {
-          final boolean matchEmpty = dp[i + 1][j];
-          final boolean matchSome = dp[i][j + 1];
-          dp[i + 1][j + 1] = matchEmpty || matchSome;
-        } else if (isMatch(s, i, p, j)) {
-          dp[i + 1][j + 1] = dp[i][j];
+    public boolean isMatch(String s, String p) {
+        int sIndex = 0, pIndex = 0, matchIndex = 0, starIndex = -1;
+        
+        while (sIndex < s.length()) {
+            if (pIndex < p.length() && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')) {
+                sIndex++;
+                pIndex++;
+            } else if (pIndex < p.length() && p.charAt(pIndex) == '*') {
+                starIndex = pIndex;
+                matchIndex = sIndex;
+                pIndex++;
+            } else if (starIndex != -1) {
+                pIndex = starIndex + 1;
+                matchIndex++;
+                sIndex = matchIndex;
+            } else {
+                return false;
+            }
         }
-
-    return dp[m][n];
-  }
-
-  private boolean isMatch(final String s, int i, final String p, int j) {
-    return j >= 0 && p.charAt(j) == '?' || s.charAt(i) == p.charAt(j);
-  }
+        
+        while (pIndex < p.length() && p.charAt(pIndex) == '*') {
+            pIndex++;
+        }
+        
+        return pIndex == p.length();
+    }
 }
