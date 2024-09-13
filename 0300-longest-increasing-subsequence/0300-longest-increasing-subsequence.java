83c79fd2e3b4a56a5d35084ccf61248bed0bbdd5
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-09-14 03:25:18 +0530

    Time: 6 ms (88.65%), Space: 44 MB (79.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -14,15 +14,15 @@ class Solution {
         }return res.size();
     }
 
-    private int binary(List<Integer> res,int target){
+    private int binary(List<Integer> res,int n){
         int left=0;
         int right=res.size()-1;
         while (left<=right){
             int mid=(left+right)/2;
-            if(res.get(mid)==target){
+            if(res.get(mid)==n){
                 return mid;
 
-            }else if(res.get(mid)>target){
+            }else if(res.get(mid)>n){
                 right=mid-1;
             }
             else {
