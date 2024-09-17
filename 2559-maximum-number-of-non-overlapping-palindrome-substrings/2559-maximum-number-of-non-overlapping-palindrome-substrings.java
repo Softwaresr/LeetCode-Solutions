45ac9ef241a0e0ddec54998476b86ba12dd409d2
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-09-17 18:54:59 +0530

    Time: 1 ms (100%), Space: 41.8 MB (51%) - LeetHub

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/2559-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/2559-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,39 +1,31 @@
 class Solution {
-    public int maxPalindromes(String s, int min) {
-        char[] cs = s.toCharArray();
-        int n = cs.length;
-        int[] odd = new int[n], even = new int[n], dp = new int[n + 1];
-        for (int i = 0, l = 0, r = -1; i < n; ++i) {
-            int k = i > r ? 1 : Math.min(odd[l + r - i], r - i + 1);
-            while (0 <= i - k && i + k < n && cs[i - k] == cs[i + k]) k++;
-            odd[i] = k--;
-            if (r < i + k) {
-                r = i + k;
-                l = i - k;
+    public int maxPalindromes(String s, int k) {
+        int count = 0;
+        int idx = 0;
+        
+        // greedily check for palindromes of length k or k + 1
+        while (idx <= s.length() - k) {
+            if (isPalindrome(s, idx, idx + k - 1)) {
+                count++;
+                idx += k;
+            } else if (idx < s.length() - k && isPalindrome(s, idx, idx + k)) {
+                count++;
+                idx += k + 1;
+            } else {
+                idx++;
             }
         }
-        for (int i = 0, l = 0, r = -1; i < n; ++i) {
-            int k = i > r ? 0 : Math.min(even[l + r - i + 1], r - i + 1);
-            while (0 <= i - k - 1 && i + k < n && cs[i - k - 1] == cs[i + k]) k++;
-            even[i] = k--;
-            if (r < i + k) {
-                r = i + k;
-                l = i - k - 1;
+        return count;
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
             }
-        }   
-        for (int i = n - 1; 0 <= i; --i) {
-            int diff = odd[i] * 2 - 1 - min;
-            if (0 <= diff) {
-                diff /= 2;  
-                dp[i - odd[i] + 1 + diff] = Math.max(dp[i - odd[i] + 1 + diff], 1 + dp[i + odd[i] - diff]);
-            }
-            diff = even[i] * 2 - min;
-            if (0 <= diff) {
-                diff /= 2;
-                dp[i - even[i] + diff] = Math.max(dp[i - even[i] + diff], 1 + dp[i + even[i] - diff]);
-            }
-            dp[i] = Math.max(dp[i], dp[i + 1]);
+            left++;
+            right--;
         }
-        return dp[0];
+        return true;
     }
 }
