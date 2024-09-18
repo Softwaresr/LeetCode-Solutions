932ed96a01194ad584f12c67727a42481caad3c9
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-09-18 19:57:12 +0530

    Time: 2 ms (99.91%), Space: 44.7 MB (64.39%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,46 +1,34 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) {
-            return "";
+        int[] tElements = new int[123];
+        for(char c: t.toCharArray()){
+            tElements[c]++;
         }
 
-        Map<Character, Integer> charCount = new HashMap<>();
-        for (char ch : t.toCharArray()) {
-            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
-        }
+        int left = 0, right = 0;
+        int find = t.length();
+        char[] arr = s.toCharArray();
 
-        int targetCharsRemaining = t.length();
-        int[] minWindow = {0, Integer.MAX_VALUE};
-        int startIndex = 0;
+        int start = 0;
+        int minLength = Integer.MAX_VALUE;
 
-        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
-            char ch = s.charAt(endIndex);
-            if (charCount.containsKey(ch) && charCount.get(ch) > 0) {
-                targetCharsRemaining--;
-            }
-            charCount.put(ch, charCount.getOrDefault(ch, 0) - 1);
+        while(right < arr.length){
+            if(tElements[arr[right]] > 0) find--;
+            tElements[arr[right++]]--;
 
-            if (targetCharsRemaining == 0) {
-                while (true) {
-                    char charAtStart = s.charAt(startIndex);
-                    if (charCount.containsKey(charAtStart) && charCount.get(charAtStart) == 0) {
-                        break;
-                    }
-                    charCount.put(charAtStart, charCount.getOrDefault(charAtStart, 0) + 1);
-                    startIndex++;
+            while(find == 0){
+                if(right - left < minLength){
+                    start = left;
+                    minLength = right - left;
                 }
 
-                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
-                    minWindow[0] = startIndex;
-                    minWindow[1] = endIndex;
-                }
-
-                charCount.put(s.charAt(startIndex), charCount.getOrDefault(s.charAt(startIndex), 0) + 1);
-                targetCharsRemaining++;
-                startIndex++;
+                if(tElements[arr[left]] == 0) find++;
+                tElements[arr[left]]++;
+                left++;
             }
         }
 
-        return minWindow[1] >= s.length() ? "" : s.substring(minWindow[0], minWindow[1] + 1);        
+        if(minLength == Integer.MAX_VALUE) return "";
+        return s.substring(start, start+minLength);
     }
 }
