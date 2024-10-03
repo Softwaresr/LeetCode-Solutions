7c1782300462655883cae09b28affd88ce6e638d
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-10-03 22:03:15 +0530

    Time: 31 ms (98.41%), Space: 52.8 MB (91.24%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -1,28 +1,54 @@
 class Solution {
-    public record Key(int idx, long ln, long prevDiff, int size){}
-    // ln : last number in the current arithmetic sequence
-    public int solve(int[] nums, int idx, long ln, long prevDiff, int size, HashMap<Key, Integer> map){
-        if(idx >= nums.length) return 0;
-        else if(map.containsKey(new Key(idx, ln, prevDiff, size))){
-            return map.get(new Key(idx, ln, prevDiff, size));
+  public int numberOfArithmeticSlices(int[] nums) {
+    int n = nums.length;
+    Map<Integer,List<Integer>> nextMap = new HashMap();
+    for(int i=0;i<n;i++){
+      List<Integer> nexts = nextMap.get(nums[i]);
+      if(nexts==null){
+        nexts = new ArrayList<>();
+        nextMap.put(nums[i],nexts);
+      }
+      nexts.add(i);
+    }   
+
+    int[][] dp = new int[n][n];
+    int res = 0;
+    for(int i=1;i<n;i++){
+      for(int j=i+1;j<n;j++){
+        long diff = (long)nums[j]-(long)nums[i];
+        // if(diff>Integer.MAX_VALUE||diff<Integer.MIN_VALUE){
+        //   continue;
+        // }
+        long next = 2L*nums[i]-nums[j];
+        if(next>Integer.MAX_VALUE||next<Integer.MIN_VALUE){
+          continue;
         }
-        int count = 0;
-        for(int i = idx; i < nums.length; i++){
-            if(size == 0){
-                count += solve(nums, i + 1, nums[i], 0, size + 1, map);
-            }
-            else if(size == 1){
-                count += solve(nums, i + 1, nums[i], nums[i] - ln, size + 1, map);
-            }
-            else if(size >= 2){
-                long currDiff = nums[i] - ln;
-                if(currDiff == prevDiff) count += 1 + solve(nums, i + 1, nums[i], nums[i] - ln, size + 1, map);
-            }
+        List<Integer> nexts = nextMap.get((int)next);
+        if(nexts==null){
+          continue;
         }
-        map.put(new Key(idx, ln, prevDiff, size), count);
-        return count;
+        int p1 = 0;
+        int p2 = nexts.size()-1;
+        int p = -1;
+        while(p1<=p2){
+          int mid = (p1+p2)/2;
+          if(nexts.get(mid)<i){
+            p = mid;
+            p1 = mid+1;
+          }else{
+            p2 = mid-1;
+          }
+        }
+        for(int k=0;k<=p;k++){
+          dp[i][j]+=dp[nexts.get(k)][i]+1;
+          // System.out.println(k+","+i+","+j+" add:");
+        }
+        res+=dp[i][j];
+      }
     }
-    public int numberOfArithmeticSlices(int[] nums) {
-        return solve(nums, 0, 0, 0l, 0, new HashMap<>());
-    }
+    // for(int i=0;i<dp.length;i++){
+    //   System.out.println(i+" "+Arrays.toString(dp[i]));
+    // }
+    return res;
+  }
 }
