d3c96d1eefd57f4a88b761dccb4e4c769f37f433
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-11-07 12:38:34 +0530

    Time: 546 ms (5.88%), Space: 113.4 MB (76.79%) - LeetHub

--- 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.java
+++ 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.java
@@ -1,37 +1,38 @@
 class Solution {
+    private List<int[]>[] g;
+    private int[] ans;
+
     public int[] minEdgeReversals(int n, int[][] edges) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) graph.put(i, new ArrayList<>());
-        for (int[] edge : edges) {
-            graph.get(edge[0]).add(new int[]{edge[1], 0}); // normal edge, weight 0
-            graph.get(edge[1]).add(new int[]{edge[0], 1}); // reverse edge, weight 1    
+        ans = new int[n];
+        g = new List[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        for (var e : edges) {
+            int x = e[0], y = e[1];
+            g[x].add(new int[] {y, 1});
+            g[y].add(new int[] {x, -1});
         }
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        dp[0] = dfs1(0, -1, graph);
-        dfs2(0, -1, graph, dp);
-        return dp;
+        dfs(0, -1);
+        dfs2(0, -1);
+        return ans;
     }
-    
-    private int dfs1(int u, int fa, Map<Integer, List<int[]>> graph) {
-        int res = 0;
-        for (int[] next : graph.get(u)) {
-            int v = next[0], w = next[1];
-            if (v == fa) continue;
-            
-            res += dfs1(v, u, graph) + w;
+
+    private void dfs(int i, int fa) {
+        for (var ne : g[i]) {
+            int j = ne[0], k = ne[1];
+            if (j != fa) {
+                ans[0] += k < 0 ? 1 : 0;
+                dfs(j, i);
+            }
         }
-        return res;
     }
-    
-    private void dfs2(int u, int fa, Map<Integer, List<int[]>> graph, int[] dp) {
-        for (int[] next : graph.get(u)) {
-            int v = next[0], w = next[1];
-            if (v == fa) continue;
-            dp[v] = dp[u];
-            if (w == 1) dp[v]--;
-            else dp[v]++;
-            dfs2(v, u, graph, dp);
+
+    private void dfs2(int i, int fa) {
+        for (var ne : g[i]) {
+            int j = ne[0], k = ne[1];
+            if (j != fa) {
+                ans[j] = ans[i] + k;
+                dfs2(j, i);
+            }
         }
     }
 }
