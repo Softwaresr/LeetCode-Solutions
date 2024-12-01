5aaa3c1f8114da8f9a4b8dc9b62eb391b0b93a87
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-12-01 16:44:12 +0530

    Time: 3 ms (16.4%), Space: 41.9 MB (10.84%) - LeetHub

--- 1252-break-a-palindrome/1252-break-a-palindrome.java
+++ 1252-break-a-palindrome/1252-break-a-palindrome.java
@@ -5,15 +5,9 @@ class Solution {
         if(p.charAt(0)!='a'){
             return 'a'+p.substring(1);
         }
-        boolean allA=true;
-        for(int i=0;i<p.length();i++){
-            if(p.charAt(i)!='a'){
-                allA=false;break;
-
-            }
         
-        }
-        if(allA==true) return p.substring(0,p.length()-1)+'b';
+        
+        
         for(int i=0;i<p.length()/2;i++){
             if(p.charAt(i)!='a'){
                 return p.substring(0,i)+'a'+p.substring(i+1,p.length());
