1182edf9547746dbc371f0e40f310e0b7e027fe5
Softwaresr <125075523+Softwaresr@users.noreply.github.com>
2024-12-10 23:14:41 +0530

    Time: 0 ms (100%), Space: 44.9 MB (93.83%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            res ^= i;
+        }
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
