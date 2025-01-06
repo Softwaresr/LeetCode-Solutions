f16fe674040e34b6f48ca6d2c85a51aef09a58bb
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-01-06 23:06:00 +0530

    Time: 16 ms (96.55%), Space: 56.3 MB (97.7%) - LeetHub

--- 2689-rearranging-fruits/2689-rearranging-fruits.java
+++ 2689-rearranging-fruits/2689-rearranging-fruits.java
@@ -1,28 +1,99 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
-        Map<Integer, Integer> cnt = new HashMap<>();
-        for (int c: basket1) 
-            cnt.put(c, cnt.getOrDefault(c, 0) + 1);
-        for (int c: basket2) 
-            cnt.put(c, cnt.getOrDefault(c, 0) - 1);
-        List<Integer> last = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> e: cnt.entrySet()) {
-            int v = e.getValue();
-            // if v is odd, an even distribution is never possible
-            if (v % 2 != 0) return -1;
-            // the count of transferred k is |v|/2
-            for (int i = 0; i < Math.abs(v) / 2; ++i)
-                last.add(e.getKey());
+        // basket1 = [4,2,2,2], basket2 = [1,4,1,2]
+        /**
+            2 -> 3
+            4 -> 1
+
+            1 -> 2
+            2 -> 1
+            4 -> 1
+         */
+
+        Arrays.sort(basket1);
+        Arrays.sort(basket2);
+       
+
+        int i = 0;
+        int j = 0;
+        int n = basket1.length;
+        int movesA = 0;
+        int movesB = 0;
+        long finalAns = 0;
+        List<Integer> list = new ArrayList<>();
+        while(i < n && j < n) {
+            int count1 = 0;
+            int count2 = 0;
+            int currEle1 = basket1[i];
+            int currEle2 = basket2[j];
+
+            if(currEle1 < currEle2) {
+                while(i < n && currEle1 == basket1[i]) {
+                    i++;
+                    count1++;
+                }
+            } else if(currEle2 < currEle1) {
+                while(j < n && currEle2 == basket2[j]) {
+                    j++;
+                    count2++;
+                }
+            } else {
+                while(i < n && j < n && basket1[i] == basket2[j]) {
+                    i++;
+                    j++;
+                }
+            }
+
+            if(currEle1 != currEle2) {
+                if(count1 % 2 != 0 || count2 % 2 != 0) {
+                    return -1;
+                } else {
+                    if(currEle1 < currEle2) {
+                        movesB = movesB + count1 / 2;
+                        finalAns += currEle1 * (count1 / 2);
+                        for(int k = 0; k < count1 / 2; k++) {
+                            list.add(currEle1);
+                        }
+                        // System.out.println("ff1: " + finalAns);
+                    } else if(currEle2 < currEle1) {
+                        movesA = movesA + count2 / 2;
+                        finalAns += currEle2 * (count2 / 2);
+                        for(int k = 0; k < count2 / 2; k++) {
+                            list.add(currEle2);
+                        }
+                        // System.out.println("ff2: " + finalAns);
+                    }
+                }
+            } 
         }
-        // find the min of two input arrays as the intermediate
-        int minx = Math.min(Arrays.stream(basket1).min().getAsInt(),
-                            Arrays.stream(basket2).min().getAsInt());
-        // Use quickselect instead of sort can get a better complexity
-        Collections.sort(last);
-        long res = 0;
-        // The first half may be the cost
-        for (int i = 0; i < last.size() / 2; ++i) 
-            res += Math.min(last.get(i), minx * 2);
-        return res;
+        if(i < n) {
+            int currEle1 = basket1[i];
+            int count1 = n-i;
+            if(count1 % 2 != 0) {
+                return -1;
+            }
+            movesB = movesB + count1 / 2;
+            // finalAns += currEle1 * (count1 / 2);
+        }
+        if(j < n) {
+            int currEle2 = basket2[j];
+            int count2 = n-j;
+            if(count2 % 2 != 0) {
+                return -1;
+            }
+            movesA = movesA + count2 / 2;
+            // finalAns += currEle2 * (count2 / 2);
+        }
+        // System.out.println("i: " + i + ", j: " + j);
+        // System.out.println("movesA: " + movesA + ", movesB: " + movesB);
+        if(movesA != movesB) {
+            return -1;
+        }
+        long ans = 0;
+        
+        for(int k = 0; k < movesA; k++) {
+            ans += Math.min((long)Math.min(basket1[0], basket2[0]) * 2, list.get(k));
+        }
+        return ans;
     }
 }
