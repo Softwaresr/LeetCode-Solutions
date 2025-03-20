ba54a5b94fe512957cf0f4499a05943eadca9224
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-03-21 00:04:51 +0530

    Time: 1 ms (99.7%), Space: 63.7 MB (10.41%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -1,33 +1,30 @@
 import java.util.*;
-class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;  // If the tree is empty, return depth 0
+class Solution{
+    public int minDepth(TreeNode root){
+        if(root == null) return 0;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        int depth = 1;  // Start with depth 1 as we are at the root
-        
-        while (!queue.isEmpty()) {
+        int depth = 1;
+        while(!queue.isEmpty()){
             int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            for (int i=0; i<size;i++){
                 TreeNode node = queue.poll();
-                
-                // If it's a leaf node, return the current depth
-                if (node.left == null && node.right == null) {
+
+                if(node.left == null && node.right == null){
                     return depth;
+
                 }
-                
-                // Add left and right children to the queue, if they exist
-                if (node.left != null) {
+                if(node.left != null){
                     queue.add(node.left);
+
                 }
-                if (node.right != null) {
+                if(node.right != null){
                     queue.add(node.right);
                 }
             }
             depth++;
         }
-        
-        return 0;  // This line will never be reached if the tree is valid
+        return 0;
     }
 }
