811a2637003ed04e41a805089b84a036e44ac156
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-06-14 22:33:34 +0530

    Time: 1 ms (89.2%), Space: 40.8 MB (40.91%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -4,7 +4,7 @@ class Solution {
         char[] maxChars = str.toCharArray();
         char[] minChars = str.toCharArray();
 
-        // Step 1: Find first non-9 digit for max replacement
+        
         char replaceForMax = ' ';
         for (char c : maxChars) {
             if (c != '9') {
@@ -13,14 +13,14 @@ class Solution {
             }
         }
 
-        // Step 2: Replace that digit with 9 for max
+        
         for (int i = 0; i < maxChars.length; i++) {
             if (maxChars[i] == replaceForMax) {
                 maxChars[i] = '9';
             }
         }
 
-        // Step 3: Replace first digit for min with 0
+        
         char replaceForMin = minChars[0];
         for (int i = 0; i < minChars.length; i++) {
             if (minChars[i] == replaceForMin) {
@@ -28,7 +28,7 @@ class Solution {
             }
         }
 
-        // Step 4: Convert and calculate result
+        
         int maxVal = Integer.parseInt(new String(maxChars));
         int minVal = Integer.parseInt(new String(minChars));
 
