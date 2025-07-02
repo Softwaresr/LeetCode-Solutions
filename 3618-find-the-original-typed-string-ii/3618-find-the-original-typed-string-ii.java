572872f9bc95845c923574d7a86449c9ef71e65a
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-07-02 23:49:53 +0530

    Time: 181 ms (25%), Space: 55.7 MB (69.44%) - LeetHub

--- 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.java
+++ 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.java
@@ -1,50 +1,40 @@
-class Solution {
-
-    private static final int mod = 1000000007;
+public class Solution {
+    private static final int MOD = (int)1e9 + 7;
 
     public int possibleStringCount(String word, int k) {
-        int n = word.length();
-        int cnt = 1;
-        List<Integer> freq = new ArrayList<>();
-        for (int i = 1; i < n; ++i) {
-            if (word.charAt(i) == word.charAt(i - 1)) {
-                ++cnt;
-            } else {
-                freq.add(cnt);
-                cnt = 1;
+        if (word.isEmpty()) return 0;
+
+        List<Integer> groups = new ArrayList<>();
+        int count = 1;
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) count++;
+            else {
+                groups.add(count);
+                count = 1;
             }
         }
-        freq.add(cnt);
+        groups.add(count);
 
-        long ans = 1;
-        for (int o : freq) {
-            ans = (ans * o) % mod;
-        }
-        if (freq.size() >= k) {
-            return (int) ans;
-        }
+        long total = 1;
+        for (int num : groups) total = (total * num) % MOD;
 
-        int[] f = new int[k];
-        int[] g = new int[k];
-        f[0] = 1;
-        Arrays.fill(g, 1);
-        for (int i = 0; i < freq.size(); ++i) {
-            int[] f_new = new int[k];
-            for (int j = 1; j < k; ++j) {
-                f_new[j] = g[j - 1];
-                if (j - freq.get(i) - 1 >= 0) {
-                    f_new[j] = (f_new[j] - g[j - freq.get(i) - 1] + mod) % mod;
-                }
+        if (k <= groups.size()) return (int)total;
+
+        int[] dp = new int[k];
+        dp[0] = 1;
+        for (int num : groups) {
+            int[] newDp = new int[k];
+            long sum = 0;
+            for (int s = 0; s < k; s++) {
+                if (s > 0) sum = (sum + dp[s - 1]) % MOD;
+                if (s > num) sum = (sum - dp[s - num - 1] + MOD) % MOD;
+                newDp[s] = (int)sum;
             }
-            int[] g_new = new int[k];
-            g_new[0] = f_new[0];
-            for (int j = 1; j < k; ++j) {
-                g_new[j] = (g_new[j - 1] + f_new[j]) % mod;
-            }
-            f = f_new;
-            g = g_new;
+            dp = newDp;
         }
+        long invalid = 0;
+        for (int s = groups.size(); s < k; s++) invalid = (invalid + dp[s]) % MOD;
 
-        return (int) ((ans - g[k - 1] + mod) % mod);
+        return (int)((total - invalid + MOD) % MOD);
     }
 }
