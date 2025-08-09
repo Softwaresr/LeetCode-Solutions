5cff26a59aa0de31e73b8ad47abda98eb25946b7
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-08-10 00:00:04 +0530

    Time: 1 ms (35.75%), Space: 40.9 MB (63.12%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,5 @@
 class Solution {
-  public boolean isPowerOfTwo(int n) {
-    return n >= 0 && Integer.bitCount(n) == 1;
-  }
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && Integer.bitCount(n) == 1;
+    }
 }
