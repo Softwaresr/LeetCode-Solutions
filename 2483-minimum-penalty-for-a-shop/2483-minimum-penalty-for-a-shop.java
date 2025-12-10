0fa9978a2d2ba1ba700a366668c5dc0a9e551d56
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-12-10 23:02:59 +0530

    Time: 8 ms (95.13%), Space: 47.2 MB (10.68%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,13 +1,15 @@
 class Solution {
     public int bestClosingTime(String customers) {
         int maxScore = 0, score = 0, rightTime = -1;
-        for (int i = 0; i < customers.length(); ++i) {
+        for(int i = 0; i < customers.length(); ++i){
             score += (customers.charAt(i) == 'Y') ? 1 : -1;
-            if (score > maxScore) {
+            if(score > maxScore){
                 maxScore = score;
                 rightTime = i;
             }
+        
         }
         return rightTime + 1;
+        
     }
 }
