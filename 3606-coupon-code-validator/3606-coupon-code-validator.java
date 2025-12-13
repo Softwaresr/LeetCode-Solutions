6056103ba0be632a727a24fb52dac9047e59fdbb
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-12-14 00:03:13 +0530

    Time: 5 ms (97.27%), Space: 47.4 MB (52.73%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.java
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -1,25 +1,22 @@
 class Solution {
-    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive){
         List<String>res = new ArrayList();
-
         List<String>e = new ArrayList();
         List<String>g = new ArrayList();
         List<String>p = new ArrayList();
         List<String>r = new ArrayList();
-        
-        for(int i=0;i<isActive.length;i++){
+
+        for(int i=0; i<isActive.length; i++){
             if(isActive[i]){
-                if(businessLine[i].equals("electronics") 
-                    || businessLine[i].equals("grocery")
-                  || businessLine[i].equals("pharmacy")
-                  || businessLine[i].equals("restaurant")){
+                if(businessLine[i].equals("electronics") || businessLine[i].equals("grocery") || businessLine[i].equals("pharmacy") || businessLine[i].equals("restaurant")){
                     if(code[i].length() == 0)continue;
                     boolean found = true;
-                    for(int j=0;j<code[i].length();j++){
+                    for(int j = 0; j<code[i].length();j++){
                         char ch = code[i].charAt(j);
-                        if (!Character.isLetterOrDigit(ch) && ch != '_') {
+                        if(!Character.isLetterOrDigit(ch) && ch != '_'){
                             found = false;
                             break;
+
                         }
                     }
 
@@ -29,7 +26,7 @@ class Solution {
                         if(businessLine[i].startsWith("p"))p.add(code[i]);
                         if(businessLine[i].startsWith("r"))r.add(code[i]);
                     }
-                  }
+                }
             }
         }
         Collections.sort(e);
@@ -40,7 +37,8 @@ class Solution {
         res.addAll(g);
         res.addAll(p);
         res.addAll(r);
-        
+
         return res;
+
     }
 }
