c6dc5eb068552c2591807e4d40f6c25871ebcceb
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-12-16 01:23:39 +0530

    Time: 0 ms (100%), Space: 42.4 MB (6.61%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,10 @@
+class Solution{
+    public int sum(int num1, int num2){
+        int[] A = new int[] {num1, num2};
+        int[] prefixSum = new int[A.length+1];
+        for(int i = 0; i < A.length; i++){
+            prefixSum[i+1] = prefixSum[i] + A[i];
+        }
+        return prefixSum[A.length];
+    }
+}
