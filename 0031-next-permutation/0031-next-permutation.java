65112e494a56d980b56286168cb73512f25dbe4b
Sumriddhi Tonk <125075523+Softwaresr@users.noreply.github.com>
2025-12-26 01:58:19 +0530

    Time: 0 ms (100%), Space: 44.6 MB (59.69%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,34 +1,47 @@
 class Solution {
-  public void nextPermutation(int[] nums) {
-    final int n = nums.length;
+    public void nextPermutation(int[] nums) {
+        int ind1= -1;
+        int ind2 = -1;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                ind1 = i;
+                break;
 
-    // From back to front, find the first number < nums[i + 1].
-    int i;
-    for (i = n - 2; i >= 0; --i)
-      if (nums[i] < nums[i + 1])
-        break;
-
-    // From back to front, find the first number > nums[i], swap it with
-    // nums[i].
-    if (i >= 0)
-      for (int j = n - 1; j > i; --j)
-        if (nums[j] > nums[i]) {
-          swap(nums, i, j);
-          break;
+            }
         }
+        if(ind1 == -1){
+            reverse(nums, 0);
 
-    // Reverse nums[i + 1..n - 1].
-    reverse(nums, i + 1, n - 1);
-  }
+        }
+        else{
+            for(int i = nums.length-1; i >= 0; i--){
+                if(nums[i] > nums[ind1]){
+                    ind2 = i; 
+                    break;
 
-  private void reverse(int[] nums, int l, int r) {
-    while (l < r)
-      swap(nums, l++, r--);
-  }
+                }
+            }
+            swap(nums, ind1,ind2);
+            reverse(nums, ind1+1);
 
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
+            
+        }
+        
+    }
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+
+    }
+    void reverse(int[] nums, int start ){
+        int i = start;
+        int j = nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
 }
